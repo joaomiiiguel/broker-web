5c835d7419d66a84d67de2ab385b896f62be79ac
joaomiiiguel <joaomiguel.farias@gmail.com>
2021-01-20 16:42:53 -0300

    Pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
+import React from 'react';
 import './App.css';
+
+import imgLogo from '../src/assets/Logo.jpg'
+import capaLocal from '../src/assets/banner-capa.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Top-bar">
+          <img src={imgLogo} className="Logo-Img" />
+        </div>
+
+        <div className="Top-Banner">
+          <div className="Text-Banner">
+            <h1>Bem vindo ao</h1>
+            <h1>Vivaz - Belém</h1>
+            <p>Um empreendimento com tudo que você sempre sonhou</p>
+            <button className="btnSabia">SABIA MAIS</button>
+          </div>
+        </div>
+        <div className="Menu-Icons">
+          <img src={capaLocal} style={{ width: '90%'}} />
+          <div style={{ textAlign:'left' }}>
+            <h3>VIVAZ ESTAÇÃO BELÉM</h3>
+            <div style={{ display:'flex', justifyContent:'space-around' }}>
+            <p>Chegou o Vivaz Estação Belém. Um empreendimento com tudo que você sempre sonhou, em uma localização especial e com todas as facilidades do Minha Casa Minha Vida. Apartamentos com 1 ou 2 quartos, opção de vaga, opção de varanda</p>
+            </div>
+          </div>
+        </div>
       </header>
+      <div className="Video-Local">
+
+      </div>
     </div>
   );
 }
